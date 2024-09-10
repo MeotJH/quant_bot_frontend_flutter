728ac2a6b23d09c129a9659bd9c64497da9202df
Meotjh <marionette934@gmail.com>
2024-09-11 00:40:18 +0900

    BUILD url_strategy lib 변경한 파일

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quant_bot_flutter/pages/stocks_page/stocks_page.dart';
 import 'package:quant_bot_flutter/providers/router_provider.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(const ProviderScope(child: QuantBot()));
 }
 
