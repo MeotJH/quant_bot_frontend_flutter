9aec7025866f343349bd3019fdf4727da1401eb5
Meotjh <marionette934@gmail.com>
2024-11-10 11:22:22 +0900

    FIX 회원가입 유효성 인증 에러

--- lib/providers/sign_up_provider.dart
+++ lib/providers/sign_up_provider.dart
@@ -91,6 +91,7 @@ class SignUpNotifier extends AutoDisposeFamilyAsyncNotifier<void, SignUpModel> {
   @override
   Future<void> build(SignUpModel model) async {
     final dio = ref.read(dioProvider);
+    debugPrint('signUpProvider ::: ${model.toJson()}');
     await SignUpService(model: model).signUp(dio: dio);
   }
 }
--- lib/services/sign_up_service.dart
+++ lib/services/sign_up_service.dart
@@ -35,16 +35,16 @@ class SignUpService {
 
   void validate() {
     final fields = {
-      '이메일': model.email,
-      '비밀번호': model.password,
-      '이름': model.userName,
-      '전화번호': model.mobile,
+      '이메일이': model.email,
+      '비밀번호가': model.password,
+      '이름이': model.userName,
+      '전화번호가': model.mobile,
     };
 
     // 빈 필드가 있는지 체크하고 첫 번째로 빈 필드에 대한 오류 메시지를 출력
     for (var entry in fields.entries) {
       if (entry.value.isEmpty) {
-        CustomToast.show(message: '${entry.key}이(가) 올바르지 않습니다.', isWarn: true);
+        CustomToast.show(message: '${entry.key} 올바르지 않습니다.', isWarn: true);
         return;
       }
     }
